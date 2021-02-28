d56706d88d387a4795ed7e4b903c6f2011198585
Alex Lipinsky <62192604+alex-lip@users.noreply.github.com>
2021-02-28 10:57:51 -0500

    Added new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+// TypeScript Decorator
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-crash-todolist';
+  title = 'Todo List';
+
 }
