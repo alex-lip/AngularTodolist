3a820b1d919db25aa17cc6d24ffc33c0f6ad7556
Alex Lipinsky <62192604+alex-lip@users.noreply.github.com>
2021-03-01 06:08:14 -0500

    Fixed errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule, httpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
