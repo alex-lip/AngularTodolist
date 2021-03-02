f05120df05aca1941558a953cd3640115a61a376
Alex Lipinsky <62192604+alex-lip@users.noreply.github.com>
2021-03-02 06:45:33 -0500

    Add Todo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { TodosComponent } from './components/todos/todos.component';
+import { TodosComponent } from './component/todos/todos.component';
 import { TodoItemComponent } from './component/todo-item/todo-item.component';
 import { HeaderComponent } from './component/layout/header/header.component';
+import { AddTodoComponent } from './component/add-todo/add-todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
     TodoItemComponent,
-    HeaderComponent
+    HeaderComponent,
+    AddTodoComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
